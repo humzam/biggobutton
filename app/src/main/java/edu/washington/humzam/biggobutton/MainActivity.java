4c82eae99930a3d5aae4b68f984fb00382efb29e
Humza Mangrio <humzam99@gmail.com>
2015-04-16 16:06:36 -0700

    added android screenshot

--- app/src/main/java/edu/washington/humzam/biggobutton/MainActivity.java
+++ app/src/main/java/edu/washington/humzam/biggobutton/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ActionBarActivity {
         pushButton = (Button) findViewById(R.id.btn_push);
         pushButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 if (pushButton.getText().equals("Push Me!")) {
                     count++;
                     pushButton.setText("You have pushed me " + count + " times!");
@@ -31,28 +31,6 @@ public class MainActivity extends ActionBarActivity {
                     pushButton.setText("You have pushed me " + count + " times!");
                 }
             }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        });
     }
 }
