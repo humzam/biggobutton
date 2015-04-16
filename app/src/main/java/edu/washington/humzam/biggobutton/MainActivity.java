bde5e31beebc4c5d5b069146367d5b689e07c1fc
Humza Mangrio <humzam99@gmail.com>
2015-04-16 14:30:52 -0700

    added event handling

--- app/src/main/java/edu/washington/humzam/biggobutton/MainActivity.java
+++ app/src/main/java/edu/washington/humzam/biggobutton/MainActivity.java
@@ -4,15 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button pushButton;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        count = 0;
+
+        pushButton = (Button) findViewById(R.id.btn_push);
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (pushButton.getText().equals("Push Me!")) {
+                    count++;
+                    pushButton.setText("You have pushed me " + count + " times!");
+                } else {
+                    count++;
+                    pushButton.setText("You have pushed me " + count + " times!");
+                }
+            }
+
 
 
     @Override
